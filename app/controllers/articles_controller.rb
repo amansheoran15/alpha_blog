b48b1bace76fc35d63f507debc6410a0e13f2e57
Aman <amansheo@gmail.com>
2024-01-11 13:36:04 +0530

    Add create new article and display flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,5 +9,24 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article has been saved!"
+      redirect_to article_path(@article), notice: "Article has been saved!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
+
 end
 
