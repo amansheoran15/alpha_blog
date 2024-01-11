3fb5453b48b0842bf3fc6cd777e42d1f33cc856c
Aman <amansheo@gmail.com>
2024-01-11 13:57:24 +0530

    Add edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -20,6 +24,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article), notice: "Article has been saved!"
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
