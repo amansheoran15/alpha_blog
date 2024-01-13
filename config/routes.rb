c7eb6987d51477081e2d0a9ec8bbdddcc7d259b9
Aman <amansheo@gmail.com>
2024-01-13 14:46:41 +0530

    Add new user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
+
   get "/about", to:"pages#about"
   # Defines the root path route ("/")
   root "pages#home"
