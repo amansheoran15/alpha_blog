9e9d36599c55851cfd4cec8c32b443ed8d339a7f
Aman <amansheo@gmail.com>
2024-01-15 15:10:39 +0530

    Change ArticlesController

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # Shifted to application_controller
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 
-  def logged_in?
-    !!current_user
-  end
+  # Shifted to application_controller
+  # def logged_in?
+  #   !!current_user
+  # end
 end
